624d472b0137ce0293039c3cc24fb266fe09193c
n313893254 <n313893254@126.com>
2018-08-15 19:02:51 +0800

    Add crypto-js

--- app/components/event/event-json/component.js
+++ app/components/event/event-json/component.js
@@ -6,8 +6,15 @@ export default Component.extend({
   layout,
   value: {},
   didInsertElement(){
+    let clone = this.value.clone()
+    delete clone.actionLinks
+    delete clone.type
+    delete clone.links
+    delete clone._super
+    delete clone.willDestroy
+    delete clone.baseType
 
-    const formatter = new window.JSONFormatter(this.value, 0);
+    const formatter = new window.JSONFormatter(clone, 1, {theme: 'dark'});
     let v = formatter.render();
 
     this.$().append(v);
