19b9bcabacade70a2aa9c98f13be156f1d4230bf
n313893254 <n313893254@126.com>
2018-08-29 17:43:26 +0800

    Auto scaler advanced section

--- app/components/hook/new-edit-receiver/component.js
+++ app/components/hook/new-edit-receiver/component.js
@@ -25,6 +25,7 @@ export default Component.extend(NewOrEdit, {
   }],
   receiver:        alias('model.receiver'),
   primaryResource: alias('model.receiver'),
+  advanced:        false,
 
   workloadContent: computed('model.workloads.[]', function() {
 
