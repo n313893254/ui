6834d4adefa29e1cf18c8ac1993c82f3379b3ef0
n313893254 <n313893254@126.com>
2018-09-30 10:18:27 +0800

    Change k8sStore url

--- app/instance-initializers/k8s-store.js
+++ app/instance-initializers/k8s-store.js
@@ -5,18 +5,21 @@ export function initialize(instance) {
 
   var k8sStore = instance.lookup('service:k8sStore');
   var cookies = instance.lookup('service:cookies');
+  var application = instance.lookup('application:main');
 
   k8sStore.reopen(StoreTweaks);
 
-  if (ENV.environment === 'development') {
+  // if (ENV.environment === 'development') {
+  //
+  //   k8sStore.baseUrl = `/k8s/clusters/local/api/v1/namespaces/cattle-system/services/https:cattle-cce-service:443/proxy`
+  //
+  // } else {
+  //
+  //   k8sStore.baseUrl = `/meta/proxy/http:/cattle-cce-service`
+  //
+  // }
 
-    k8sStore.baseUrl = `/k8s/clusters/local/api/v1/namespaces/cattle-system/services/https:cattle-cce-service:443/proxy`
-
-  } else {
-
-    k8sStore.baseUrl = `/meta/proxy/http:/cattle-cce-service`
-
-  }
+  k8sStore.baseUrl = application.apiEndpoint
 
   let timeout = cookies.get('timeout');
 
