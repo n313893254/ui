a38b40e978cb976458b04020e32e78a3b19b85fa
n313893254 <n313893254@126.com>
2019-01-14 17:41:30 +0800

    Add zoomlion auth

--- app/verify-auth-zoomlion/route.js
+++ app/verify-auth-zoomlion/route.js
@@ -2,14 +2,42 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
-import C from 'shared/utils/constants';
+import { addQueryParams, parseUrl } from 'shared/utils/util';
+import { reject } from 'rsvp';
 import VerifyAuth from 'ui/mixins/verify-auth';
+
+const allowedForwards = ['localhost'];
 
 export default Route.extend(VerifyAuth, {
   globalStore: service(),
+  zoomlion:    service(),
 
   model(params/* , transition */) {
-    const globalStore = get(this, 'globalStore')
+    const forward = get(params, 'forward');
+    const code    = get(params, 'code');
+    const zoomlion  = get(this, 'zoomlion');
+
+    // Allow another redirect if the hostname is in the whitelist above.
+    // This allows things like sharing github auth between rancher at localhost:8000
+    // and rio dev at localhost:8004
+    if ( forward ) {
+      const parsed = parseUrl(forward);
+
+      if ( allowedForwards.includes(parsed.hostname.toLowerCase()) ) {
+        if ( get(params, 'login') ) {
+          window.location.href = addQueryParams(forward, {
+            forwarded: 'true',
+            code
+          });
+        } else {
+          zoomlion.login(forward);
+        }
+      } else {
+        return reject(new Error('Invalid forward url'));
+      }
+
+      return;
+    }
 
     if (window.opener && !get(params, 'login')) {
       if (get(params, 'code')) {
--- app/verify-login-zoomlion/route.js
+++ app/verify-login-zoomlion/route.js
@@ -1,18 +1,13 @@
 import Route from '@ember/routing/route';
-import RSVP from 'rsvp';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 import C from 'shared/utils/constants';
 import VerifyAuth from 'ui/mixins/verify-auth';
 
-const samlProviders = ['ping', 'adfs', 'keycloak'];
-
 export default Route.extend(VerifyAuth, {
   globalStore: service(),
 
   model(params/* , transition */) {
-    const globalStore = get(this, 'globalStore')
-
     if (get(params, 'code')) {
       let ghProvider = get(this, 'access.providers').findBy('id', 'zoomlion');
 
@@ -25,19 +20,6 @@ export default Route.extend(VerifyAuth, {
         return get(this, 'access').detect()
           .then(() => this.transitionTo('authenticated'));
       });
-    }
-
-    function reply(err, code) {
-      try {
-        window.opener.window.onAuthTest(err, code);
-        setTimeout(() => {
-          window.close();
-        }, 250);
-
-        return new RSVP.promise();
-      } catch (e) {
-        window.close();
-      }
     }
   }
 
