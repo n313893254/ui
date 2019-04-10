d3dfe5a58fa734bbae2dc9222b6f368d4be6ab8d
n313893254 <n313893254@126.com>
2019-04-10 15:00:43 +0800

    poi

--- app/components/modal-import/component.js
+++ app/components/modal-import/component.js
@@ -6,6 +6,8 @@ import jsyaml from 'js-yaml';
 import ModalBase from 'shared/mixins/modal-base';
 import layout from './template';
 import ChildHook from 'shared/mixins/child-hook';
+import json2yaml from 'json2yaml';
+import dotObject from 'dot-object';
 
 export default Component.extend(ModalBase, ChildHook, {
   intl:  service(),
@@ -94,14 +96,116 @@ export default Component.extend(ModalBase, ChildHook, {
   lintObserver: observer('yaml', function() {
     const yaml = get(this, 'yaml');
     const lintError = [];
+    try {
+      const poi = jsyaml.safeLoad(yaml);
+      const answers = this.toAnswer(poi)
+      const newObj = dotObject.dot(answers)
+      console.log(JSON.stringify(newObj), 'newObj')
+      const expandObj = this.expand(newObj)
+      // const expandObj = dotObject.object(newObj)
+      console.log(expandObj, 'expandObj')
+      const out = json2yaml.stringify(expandObj)
+      console.log(out, 'out')
+    } catch (e) {
+      console.log(e, 'error')
+    } finally {
 
-    jsyaml.safeLoadAll(yaml, (y) => {
-      lintError.pushObjects(CodeMirror.lint.yaml(y));
-    });
-
-    if ( lintError.length ) {
-      set(this, 'errors', null);
     }
   }),
+
+  toAnswer(obj) {
+    const keys = Object.keys(obj)
+    let out = {}
+    keys.map((key='') => {
+      let dotKey = key
+      if (key.includes('.')) {
+        // dotKey = `"${key}"`
+        dotKey = key.replace(/[^\\]\./g, '\\.')
+      }
+      if (typeOf(obj[key]) === 'string' || typeOf(obj[key]) === 'number' || typeOf(obj[key]) === 'boolean') {
+        out[dotKey] = obj[key]
+      }
+      if (Array.isArray(obj[key])) {
+        const arr = obj[key]
+        arr.map((a, index) => {
+          if (typeOf(a !== 'object')) {
+            out[`${dotKey}[${index}]`] = a
+          } else {
+            out[`${dotKey}[${index}]`] = this.toAnswer(a)
+          }
+        })
+      }
+      if (typeOf(obj[key]) === 'object') {
+        out[dotKey] = this.toAnswer(obj[key])
+      }
+    })
+
+    return out
+  },
+
+  removeSlash(obj={}) {
+    Object.keys(obj).map((key='') => {
+      key.replace(/[^\\]\./g, '\\.')
+    })
+  },
+
+  dotify(obj, current, out={}) {
+    for (let key in obj) {
+      let dotKey = key
+      if (key.includes('.')) {
+        dotKey = key.replace(/[^\\]\./g, '\.')
+      }
+      const value = obj[key]
+      const newKey = current ? current + '.' + dotKey : dotKey
+      if (value && typeof value === 'object') {
+        this.dotify(value, newKey, out);
+      } else {
+        out[`${newKey}`] = value;
+      }
+    }
+  },
+
+  expand(obj) {
+    let out = {}
+
+    Object.keys(obj).map((key='') => {
+      const paths = key.split('.')
+      const queue = this.pathMerge(paths)
+
+      queue.reduce((accumulator, value, idx) => {
+        if (idx + 1 === queue.length) {
+          accumulator[value] = obj[key]
+        } else {
+          accumulator[value] = {}
+        }
+
+        return accumulator[value]
+      }, out)
+    })
+
+    return out
+  },
+
+  pathMerge(paths) {
+    let queue = []
+    let flag = undefined
+    paths.map((p, index) => {
+      if (flag + 1 === index ) {
+        return
+      }
+      if (p.endsWith('\\') && (index + 1 !== paths.length)) {
+        flag = index
+        queue.push([p, paths[index + 1]].join('.'))
+      } else {
+        queue.push(p)
+      }
+    })
+
+    if (queue.filter((key='', idx) => key.endsWith('\\') && (idx + 1 !== queue.length)).length > 0) {
+      queue = this.pathMerge(queue)
+    }
+
+    return queue
+  },
 
 });
